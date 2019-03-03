4171e27c51f834a6640a696188104cb675bcf39a
wujun <1477563131@qq.com>
2019-03-03 22:52:40 +0800

    init  quatrz

---
+++ src/main/java/com/springboot/app/quatrz/PrintCurrentTimeTask.java
@@ -0,0 +1,16 @@
+package com.springboot.app.quatrz;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class PrintCurrentTimeTask {
+    @Scheduled(cron = "0/5 * * * * ? ")
+    public void printCurrentTime() {
+        System.out.println("Current Time is:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    }
+}
